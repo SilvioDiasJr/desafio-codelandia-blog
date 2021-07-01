fb64b966cca5ea3679b4ba870a72868ab11e09a5
Silvio <silvio.dias@etec.sp.gov.br>
2021-07-01 19:15:29 -0300

    adicionando configurações de animações

---
+++ js/animate.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000,
+  reset: true
+});
+
+ScrollReveal().reveal('.card-post', { delay: 200 });
+ScrollReveal().reveal('.card-header', { delay: 400 });
+ScrollReveal().reveal('.card-title', { delay: 500 });
+ScrollReveal().reveal('.card-description', { delay: 600 });
